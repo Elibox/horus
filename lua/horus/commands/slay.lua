02b35ddabaed87e29ba370051082a5077356fe02
Xobile <knightroonik@gmail.com>
2020-04-07 18:33:02 +1000

    teleport and multislay dont work but uh spookify and goto kinda gaming

--- lua/horus/commands/slay.lua
+++ lua/horus/commands/slay.lua
@@ -5,6 +5,17 @@ horus.command('slay', 'Eliminate a player', {'player_one'}, function(caller, tar
 	return true, '%c killed %1'
 end)
 
+--horus.command('multislay', 'Eliminate multiple players!', {'player_one', 'player_two'}, function(caller, target1, target2)
+--	if target1:Alive() then
+--		target1:Kill()
+--	end
+--	return true, '%c killed %1'
+--	if target2:Alive() then
+--		target2:Kill()
+--	end
+--	return true, '%c killed %1'
+--end)
+
 horus.command('explode', 'Detonate a player', {'player_one'}, function(caller, target)
 	if target:Alive() then
 		local boom = ents.Create('env_explosion')
@@ -14,6 +25,28 @@ horus.command('explode', 'Detonate a player', {'player_one'}, function(caller, t
 		boom:Fire('Explode')
 		target:Kill()
 	end
-
 	return true, '%c exploded %1'
+end)
+
+horus.command('spookify', 'Sets the model of a player to a skeleton', {'player_one'}, function(caller, target)
+	if target:Alive() then
+		target:SetModel('models/player/skeleton.mdl')
+	end
+	return true, '%c spookified %1'
+end)
+
+horus.command('goto', 'Goto a player', {'player_one'}, function(caller,target)
+	if target:Alive() then
+		local pos = target:GetPos()
+		caller:SetPos(pos)
+	end
+	return true, '%c went to %1'
+end)
+
+horus.command('teleport', "Teleport a player to where you're looking", {'player_one'}, function(caller,target)
+	if target:Alive() then
+		local pos = caller:GetAimVector()
+		target:SetPos(pos)
+	end
+	return true, '%c teleported %1'
 end)
